57358d51ef1917d5c4c8b0a63cf7faf71e833258
margb13 <marg.b13@hotmail.com>
2017-12-15 17:57:51 -0500

    Terminando responsive para movil y escritorio

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-// La funcionalidad de tu proyecto
+/* Funcion del API maps*/
+/*
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 41.85, lng: -87.65}
+  });
+  directionsDisplay.setMap(map);
+
+  var mySearch = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('search').addEventListener('clic', mySearch);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}*/
